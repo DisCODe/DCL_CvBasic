97750a6d093ae59b0a645134a5c6637f0446dd7e
Tomasz Kornuta <tkornuta@gmail.com>
2013-01-21 00:15:51 +0100

    not working:]

--- src/Components/CvBayesClassifier/CvBayesClassifier.cpp
+++ src/Components/CvBayesClassifier/CvBayesClassifier.cpp
@@ -118,46 +118,72 @@ void CvBayesClassifier::onTraining()
 	try {
 		// Train in here...
 		//cv::Mat img(Size(320,240),CV_8UC3)
-		cv::Mat train_mat(Size(24,training_dataset.size()),CV_32FC1);
-		cv::Mat resp_mat(Size(1,training_dataset.size()),CV_8UC1);
+		cv::Mat train_mat = cv::Mat::zeros( training_dataset.size(), 10, CV_32FC1);
+		cv::Mat resp_mat = cv::Mat::zeros( 1, training_dataset.size(), CV_32FC1);
 
 		for(unsigned int i = 0; i < training_dataset.size(); i++ ){
-			train_mat.at<float>(i, 0) = training_dataset[i].m00;
-			train_mat.at<float>(i, 1) = training_dataset[i].m10;
-			train_mat.at<float>(i, 2) = training_dataset[i].m01;
-			train_mat.at<float>(i, 3) = training_dataset[i].m20;
-			train_mat.at<float>(i, 4) = training_dataset[i].m11;
-			train_mat.at<float>(i, 5) = training_dataset[i].m02;
-			train_mat.at<float>(i, 6) = training_dataset[i].m30;
-			train_mat.at<float>(i, 7) = training_dataset[i].m21;
-			train_mat.at<float>(i, 8) = training_dataset[i].m12;
-			train_mat.at<float>(i, 9) = training_dataset[i].m03;
+/*			train_mat.at<double>(i, 0) = (double) training_dataset[i].m00;
+			train_mat.at<double>(i, 1) = (double) training_dataset[i].m10;
+			train_mat.at<double>(i, 2) = (double) training_dataset[i].m01;
+			train_mat.at<double>(i, 3) = (double) training_dataset[i].m20;
+			train_mat.at<double>(i, 4) = (double) training_dataset[i].m11;
+			train_mat.at<double>(i, 5) = (double) training_dataset[i].m02;
+			train_mat.at<double>(i, 6) = (double) training_dataset[i].m30;
+			train_mat.at<double>(i, 7) = (double) training_dataset[i].m21;
+			train_mat.at<double>(i, 8) = (double) training_dataset[i].m12;
+			train_mat.at<double>(i, 9) = (double) training_dataset[i].m03;
 			//! central moments
-			train_mat.at<float>(i, 10) = training_dataset[i].mu20;
-			train_mat.at<float>(i, 11) = training_dataset[i].mu11;
-			train_mat.at<float>(i, 12) = training_dataset[i].mu02;
-			train_mat.at<float>(i, 13) = training_dataset[i].mu30;
-			train_mat.at<float>(i, 14) = training_dataset[i].mu21;
-			train_mat.at<float>(i, 15) = training_dataset[i].mu12;
-			train_mat.at<float>(i, 16) = training_dataset[i].mu03;
+			train_mat.at<double>(i, 10) = (double) training_dataset[i].mu20;
+			train_mat.at<double>(i, 11) = (double) training_dataset[i].mu11;
+			train_mat.at<double>(i, 12) = (double) training_dataset[i].mu02;
+			train_mat.at<double>(i, 13) = (double) training_dataset[i].mu30;
+			train_mat.at<double>(i, 14) = (double) training_dataset[i].mu21;
+			train_mat.at<double>(i, 15) = (double) training_dataset[i].mu12;
+			train_mat.at<double>(i, 16) = (double) training_dataset[i].mu03;
 			//! central normalized moments
-			train_mat.at<float>(i, 17) = training_dataset[i].nu20;
-			train_mat.at<float>(i, 18) = training_dataset[i].nu11;
-			train_mat.at<float>(i, 19) = training_dataset[i].nu02;
-			train_mat.at<float>(i, 20) = training_dataset[i].nu30;
-			train_mat.at<float>(i, 21) = training_dataset[i].nu21;
-			train_mat.at<float>(i, 22) = training_dataset[i].nu12;
-			train_mat.at<float>(i, 23) = training_dataset[i].nu03;
+			train_mat.at<double>(i, 17) = (double) training_dataset[i].nu20;
+			train_mat.at<double>(i, 18) = (double) training_dataset[i].nu11;
+			train_mat.at<double>(i, 19) = (double) training_dataset[i].nu02;
+			train_mat.at<double>(i, 20) = (double) training_dataset[i].nu30;
+			train_mat.at<double>(i, 21) = (double) training_dataset[i].nu21;
+			train_mat.at<double>(i, 22) = (double) training_dataset[i].nu12;
+			train_mat.at<double>(i, 23) = (double) training_dataset[i].nu03;*/
 
 			// class
-			resp_mat.at<uchar>(i, 0) = training_responses[i];
+			resp_mat.at<double>(0,i) = (double) training_responses[i];
 		}
 
 		CLOG(LINFO) << "Training matrix:\n"<<train_mat;
 		CLOG(LINFO) << "Training response:\n"<<resp_mat;
 
 		// Train is coming! ;)
-		//bayes.train(train_mat, resp_mat, Mat(), Mat(), false);
+//		bayes.train(train_mat, resp_mat);
+
+		cv::Mat train = cv::Mat::zeros( 100, 32, CV_32FC1);//, cv::Scalar(CV_VAR_ORDERED));
+		train.at<double>(0, 0) = (double) 2;
+		train.at<double>(0, 1) = (double) 5;
+		train.at<double>(1, 17) = (double) 12;
+		train.at<double>(1, 9) = (double) 235;
+		train.at<double>(29, 1) = (double) 645;
+		train.at<double>(34, 12) = (double) 65;
+		train.at<double>(23, 3) = (double) 2.64;
+		train.at<double>(27, 8) = (double) 5443;
+		train.at<double>(3, 7) = (double) 125432;
+		train.at<double>(67, 14) = (double) 6533;
+		train.at<double>(78, 18) = (double) 43265;
+		train.at<double>(92, 12) = (double) 65.543;
+		cv::Mat res = cv::Mat::ones( 1, 100, CV_32FC1);//, cv::Scalar(CV_VAR_CATEGORICAL) );
+		res.at<double>(0, 0) = (double) 2;
+		res.at<double>(1, 0) = (double) 2;
+		res.at<double>(29, 0) = (double) 2;
+
+/*		CLOG(LINFO) << "train matrix:\n"<<train;
+		CLOG(LINFO) << "res response:\n"<<res;*/
+
+		//cvSet( train, cvScalarAll(CV_VAR_ORDERED));
+		//cvSet( res, cvScalarAll(CV_VAR_CATEGORICAL));
+		bayes.train(train, res);
+		CLOG(LINFO) << "Training successful";
 
 	} catch (...) {
 		CLOG(LERROR) << "CvBayesClassifier::onTraining failed\n";
