90ac2c091884481df84f2a2179f74c41894776fc
Tomasz Kornuta <tkornuta@gmail.com>
2013-01-26 19:28:09 +0100

    working

--- src/Components/CvBayesClassifier/CvBayesClassifier.cpp
+++ src/Components/CvBayesClassifier/CvBayesClassifier.cpp
@@ -97,7 +97,6 @@ void CvBayesClassifier::onNewData()
 			add = false;
 			// TODO
 			for(std::vector<Moments>::iterator it = m.begin(); it != m.end(); ++it) {
-			    /* std::cout << *it; ... */
 				training_dataset.push_back((Moments)*it);
 				training_responses.push_back(trainingClass);
 				CLOG(LNOTICE) << "Set of moments added to training dataset";
@@ -167,9 +166,6 @@ void CvBayesClassifier::prepareSampleMatrix(const vector<Moments>& vector_, cv::
 		mat_.at<float>(i, 21) = (float) vector_[i].nu21;
 		mat_.at<float>(i, 22) = (float) vector_[i].nu12;
 		mat_.at<float>(i, 23) = (float) vector_[i].nu03;
-
-		// class
-//		resp_mat.at<float>(0,i) = (float) training_responses[i];
 	}
 
 }
@@ -189,51 +185,11 @@ void CvBayesClassifier::onTraining()
 {
 	CLOG(LTRACE) << "CvBayesClassifier::onTraining\n";
 	try {
-		// Train in here...
-		//cv::Mat img(Size(320,240),CV_8UC3)
-/*
-		prepareSampleMatrix(training_dataset, train_mat);
-		cv::Mat resp_mat;
-		prepareResponseVector(resp_mat);
-*/
 
 		cv::Mat train_mat = cv::Mat::zeros( training_dataset.size(), 24, CV_32FC1);
 		cv::Mat resp_mat = cv::Mat::zeros( 1, training_dataset.size(), CV_32FC1);
 		prepareSampleMatrix(training_dataset, train_mat);
 		prepareResponseVector(resp_mat);
-
-/*		for(unsigned int i = 0; i < training_dataset.size(); i++ ){
-			//! central moments
-			train_mat.at<float>(i, 0) = (float) training_dataset[i].m00;
-			train_mat.at<float>(i, 1) = (float) training_dataset[i].m10;
-			train_mat.at<float>(i, 2) = (float) training_dataset[i].m01;
-			train_mat.at<float>(i, 3) = (float) training_dataset[i].m20;
-			train_mat.at<float>(i, 4) = (float) training_dataset[i].m11;
-			train_mat.at<float>(i, 5) = (float) training_dataset[i].m02;
-			train_mat.at<float>(i, 6) = (float) training_dataset[i].m30;
-			train_mat.at<float>(i, 7) = (float) training_dataset[i].m21;
-			train_mat.at<float>(i, 8) = (float) training_dataset[i].m12;
-			train_mat.at<float>(i, 9) = (float) training_dataset[i].m03;
-			//! central moments
-			train_mat.at<float>(i, 10) = (float) training_dataset[i].mu20;
-			train_mat.at<float>(i, 11) = (float) training_dataset[i].mu11;
-			train_mat.at<float>(i, 12) = (float) training_dataset[i].mu02;
-			train_mat.at<float>(i, 13) = (float) training_dataset[i].mu30;
-			train_mat.at<float>(i, 14) = (float) training_dataset[i].mu21;
-			train_mat.at<float>(i, 15) = (float) training_dataset[i].mu12;
-			train_mat.at<float>(i, 16) = (float) training_dataset[i].mu03;
-			//! central normalized moments
-			train_mat.at<float>(i, 17) = (float) training_dataset[i].nu20;
-			train_mat.at<float>(i, 18) = (float) training_dataset[i].nu11;
-			train_mat.at<float>(i, 19) = (float) training_dataset[i].nu02;
-			train_mat.at<float>(i, 20) = (float) training_dataset[i].nu30;
-			train_mat.at<float>(i, 21) = (float) training_dataset[i].nu21;
-			train_mat.at<float>(i, 22) = (float) training_dataset[i].nu12;
-			train_mat.at<float>(i, 23) = (float) training_dataset[i].nu03;
-
-			// class
-			resp_mat.at<float>(0,i) = (float) training_responses[i];
-		}*/
 
 		CLOG(LINFO) << "Training matrix:\n"<<train_mat;
 		CLOG(LINFO) << "Training response:\n"<<resp_mat;
@@ -242,32 +198,8 @@ void CvBayesClassifier::onTraining()
 		bayes.train(train_mat, resp_mat);
 
 		// Clear dataset.
-//		onClearDataset();
+		onClearDataset();
 
-/*		cv::Mat train = cv::Mat::zeros( 100, 32, CV_32FC1);//, cv::Scalar(CV_VAR_ORDERED));
-		train.at<double>(0, 0) = (double) 2;
-		train.at<double>(0, 1) = (double) 5;
-		train.at<double>(1, 17) = (double) 12;
-		train.at<double>(1, 9) = (double) 235;
-		train.at<double>(29, 1) = (double) 645;
-		train.at<double>(34, 12) = (double) 65;
-		train.at<double>(23, 3) = (double) 2.64;
-		train.at<double>(27, 8) = (double) 5443;
-		train.at<double>(3, 7) = (double) 125432;
-		train.at<double>(67, 14) = (double) 6533;
-		train.at<double>(78, 18) = (double) 43265;
-		train.at<double>(92, 12) = (double) 65.543;
-		cv::Mat res = cv::Mat::ones( 1, 100, CV_32FC1);//, cv::Scalar(CV_VAR_CATEGORICAL) );
-		res.at<double>(0, 0) = (double) 2;
-		res.at<double>(1, 0) = (double) 2;
-		res.at<double>(29, 0) = (double) 2;*/
-
-/*		CLOG(LINFO) << "train matrix:\n"<<train;
-		CLOG(LINFO) << "res response:\n"<<res;*/
-
-		//cvSet( train, cvScalarAll(CV_VAR_ORDERED));
-		//cvSet( res, cvScalarAll(CV_VAR_CATEGORICAL));
-//		bayes.train(train, res);
 		CLOG(LNOTICE) << "Training successful";
 
 	} catch (...) {
