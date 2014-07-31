388f8e81e385c3cf678c61284ea8ba6d44fd3302
maciek-slon <maciek.slon@gmail.com>
2014-07-31 17:28:26 +0200

    Code formatting

--- src/Components/CvFindChessboardCorners/CvFindChessboardCorners_Processor.hpp
+++ src/Components/CvFindChessboardCorners/CvFindChessboardCorners_Processor.hpp
@@ -1,9 +1,9 @@
 /*!
-* \file CvFindChessboardCorners_Processor.hpp
-* \brief Chessboard localization component.
-* \date Oct 20, 2010
-* \author mboryn
-*/
+ * \file CvFindChessboardCorners_Processor.hpp
+ * \brief Chessboard localization component.
+ * \date Oct 20, 2010
+ * \author mboryn
+ */
 
 #ifndef CVFINDCHESSBOARDCORNERS_PROCESSOR_HPP_
 #define CVFINDCHESSBOARDCORNERS_PROCESSOR_HPP_
@@ -19,191 +19,145 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-
 /**
-* \defgroup CvFindChessboardCorners CvFindChessboardCorners
-* \ingroup Processors
-*
-* \brief Locates chessboard on the image.
-*
-* \par Data streams:
-*
-* \streamin{in_img,cv::Mat}
-* Input image.
-* \streamout{out_chessboard,Types::Objects3D::Chessboard}
-* Located chessboard.
-*
-* \par Events:
-*
-* \event{chessboardFound}
-* Chessboard has been found.
-*
-* \event{chessboardNotFound}
-* Chessboard has not been found.
-*
-*
-*
-* \par Event handlers:
-*
-* \handler{onNewImage}
-* New image arrived
-*
-* \par Properties:
-* \prop{width,int,""}
-* Chessboard width.
-* This is number of corners, not the number of fields. Number of corners = number of fields - 1.
-*
-* \prop{height,int,""}
-* Chessboard height.
-* This is number of corners, not the number of fields. Number of corners = number of fields - 1.
-*
-* \prop{squareSize,int,""}
-* Square size in meters.
-*
-* \see http://opencv.willowgarage.com/documentation/cpp/camera_calibration_and_3d_reconstruction.html#cv-findchessboardcorners
-* @{
-*
-* @}
-*/
-
+ * \defgroup CvFindChessboardCorners CvFindChessboardCorners
+ * \ingroup Processors
+ *
+ * \brief Locates chessboard on the image.
+ *
+ * \par Data streams:
+ *
+ * \streamin{in_img,cv::Mat}
+ * Input image.
+ * \streamout{out_chessboard,Types::Objects3D::Chessboard}
+ * Located chessboard.
+ *
+ * \par Events:
+ *
+ * \event{chessboardFound}
+ * Chessboard has been found.
+ *
+ * \event{chessboardNotFound}
+ * Chessboard has not been found.
+ *
+ *
+ *
+ * \par Event handlers:
+ *
+ * \handler{onNewImage}
+ * New image arrived
+ *
+ * \par Properties:
+ * \prop{width,int,""}
+ * Chessboard width.
+ * This is number of corners, not the number of fields. Number of corners = number of fields - 1.
+ *
+ * \prop{height,int,""}
+ * Chessboard height.
+ * This is number of corners, not the number of fields. Number of corners = number of fields - 1.
+ *
+ * \prop{squareSize,int,""}
+ * Square size in meters.
+ *
+ * \see http://opencv.willowgarage.com/documentation/cpp/camera_calibration_and_3d_reconstruction.html#cv-findchessboardcorners
+ * @{
+ *
+ * @}
+ */
 
 namespace Processors {
 namespace CvFindChessboardCorners {
 
 using namespace cv;
 
-
 #define ELEMS BOOST_PP_TUPLE_TO_LIST(5, (NEAREST, LINEAR, AREA, CUBIC, LANCZOS4))
 GENERATE_ENUM_TRANSLATOR(InterpolationTranslator, int, ELEMS, INTER_);
 
-/*
-
-class InterpolationTranslator {
+class CvFindChessboardCorners_Processor: public Base::Component {
 public:
-static int fromStr(const std::string & s)
-{
-if (s == "NEAREST")
-return INTER_NEAREST;
-else if (s == "LINEAR")
-return INTER_LINEAR;
-else if (s == "AREA")
-return INTER_AREA;
-else if (s == "CUBIC")
-return INTER_CUBIC;
-else if (s == "LANCZOS4")
-return INTER_LANCZOS4;
-else
-return INTER_NEAREST;
-}
+	CvFindChessboardCorners_Processor(const std::string & name = "");
+	virtual ~CvFindChessboardCorners_Processor();
 
-static std::string toStr(int t)
-{
-switch (t)
-{
-case INTER_NEAREST:
-return "NEAREST";
-case INTER_LINEAR:
-return "LINEAR";
-case INTER_AREA:
-return "AREA";
-case INTER_CUBIC:
-return "CUBIC";
-case INTER_LANCZOS4:
-return "LANCZOS4";
-default:
-return "NEAREST";
-}
-}
-};*/
-
-class CvFindChessboardCorners_Processor: public Base::Component
-{
-public:
-CvFindChessboardCorners_Processor(const std::string & name = "");
-virtual ~CvFindChessboardCorners_Processor();
-
-/*!
-* Prepares communication interface.
-*/
-virtual void prepareInterface();
-
+	/*!
+	 * Prepares communication interface.
+	 */
+	virtual void prepareInterface();
 
 protected:
-/*!
-* Method called when component is started
-* \return true on success
-*/
-virtual bool onStart();
+	/*!
+	 * Method called when component is started
+	 * \return true on success
+	 */
+	virtual bool onStart();
 
-/*!
-* Method called when component is stopped
-* \return true on success
-*/
-virtual bool onStop();
+	/*!
+	 * Method called when component is stopped
+	 * \return true on success
+	 */
+	virtual bool onStop();
 
-/*!
-* Method called when component is initialized
-* \return true on success
-*/
-virtual bool onInit();
+	/*!
+	 * Method called when component is initialized
+	 * \return true on success
+	 */
+	virtual bool onInit();
 
-/*!
-* Method called when component is finished
-* \return true on success
-*/
-virtual bool onFinish();
+	/*!
+	 * Method called when component is finished
+	 * \return true on success
+	 */
+	virtual bool onFinish();
 
-/*!
-* Method called when step is called
-* \return true on success
-*/
-virtual bool onStep();
-
+	/*!
+	 * Method called when step is called
+	 * \return true on success
+	 */
+	virtual bool onStep();
 
 private:
-void onNewImage();
+	void onNewImage();
 
-void initChessboard();
+	void initChessboard();
 
-/** New image event handler. */
-Base::EventHandler <CvFindChessboardCorners_Processor> h_onNewImage;
-/** Image stream. */
-Base::DataStreamIn <cv::Mat> in_img;
-/** Chessboard stream. */
-Base::DataStreamOut <Types::Objects3D::Chessboard> out_chessboard;
-Base::DataStreamOut <Types::ImagePosition> out_imagePosition;
+	/** New image event handler. */
+	Base::EventHandler<CvFindChessboardCorners_Processor> h_onNewImage;
+	/** Image stream. */
+	Base::DataStreamIn<cv::Mat> in_img;
+	/** Chessboard stream. */
+	Base::DataStreamOut<Types::Objects3D::Chessboard> out_chessboard;
+	Base::DataStreamOut<Types::ImagePosition> out_imagePosition;
 
-/** Located corners.*/
-std::vector<cv::Point2f> corners;
+	/** Located corners.*/
+	std::vector<cv::Point2f> corners;
 
-int findChessboardCornersFlags;
+	int findChessboardCornersFlags;
 
-Common::Timer timer;
+	Common::Timer timer;
 
-boost::shared_ptr<Types::Objects3D::Chessboard> chessboard;
+	boost::shared_ptr<Types::Objects3D::Chessboard> chessboard;
 
-cv::Mat sub_img;
+	cv::Mat sub_img;
 
-Base::Property<bool> prop_subpix;
-Base::Property<int> prop_subpix_window;
-Base::Property<bool> prop_scale;
-Base::Property<int> prop_scale_factor;
-Base::Property<int> prop_width;
-Base::Property<int> prop_height;
-Base::Property<float> prop_square_width;
-Base::Property<float> prop_square_height;
+	Base::Property<bool> prop_subpix;
+	Base::Property<int> prop_subpix_window;
+	Base::Property<bool> prop_scale;
+	Base::Property<int> prop_scale_factor;
+	Base::Property<int> prop_width;
+	Base::Property<int> prop_height;
+	Base::Property<float> prop_square_width;
+	Base::Property<float> prop_square_height;
 
-Base::Property<bool> prop_fastCheck;
-Base::Property<bool> prop_filterQuads;
-Base::Property<bool> prop_adaptiveThreshold;
-Base::Property<bool> prop_normalizeImage;
+	Base::Property<bool> prop_fastCheck;
+	Base::Property<bool> prop_filterQuads;
+	Base::Property<bool> prop_adaptiveThreshold;
+	Base::Property<bool> prop_normalizeImage;
 
-Base::Property<int, InterpolationTranslator> prop_interpolation_type;
+	Base::Property<int, InterpolationTranslator> prop_interpolation_type;
 
 // TODO: add unit types: found and not found
 
-void sizeCallback(int old_value, int new_value);
-void flagsCallback(bool old_value, bool new_value);
+	void sizeCallback(int old_value, int new_value);
+	void flagsCallback(bool old_value, bool new_value);
 };
 
 } // namespace CvFindChessboardCorners {
